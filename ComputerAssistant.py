c5d8ad4f9cccdc3da9f3617f84e7876a226b0ef4
Venom Snake <78187975+venomsnake@users.noreply.github.com>
2021-02-24 22:07:50 +0530

    ADD : Comments for better interpretation

--- ComputerAssistant.py
+++ ComputerAssistant.py
@@ -1,3 +1,5 @@
+# Importing the modules
+
 import speech_recognition as sr
 import pyttsx3
 import datetime
@@ -5,16 +7,17 @@ import pywhatkit
 import wikipedia
 import pyjokes
 
-listener = sr.Recognizer()
-engn = pyttsx3.init()
-voices = engn.getProperty('voices')
-engn.setProperty('voice', voices[1].id)
+listener = sr.Recognizer() # for listening to audio
+engn = pyttsx3.init() # Initilize the audio engine
+voices = engn.getProperty('voices') # Uses voices from
+engn.setProperty('voice', voices[1].id) # select voice of AI
 
 
 def talk(command):
     engn.say(command)
     engn.runAndWait()
-
+    
+# Command fucntion block
 
 def take_command():
     try:
@@ -29,28 +32,35 @@ def take_command():
         pass
     return command
 
-
+# function to run bot 
 def run_alexa():
     cmd = take_command()
     if 'date' in cmd:
-        dt = datetime.datetime.now().strftime('Today\'s date is %d %B %Y')
+        dt = datetime.datetime.now().strftime('Today\'s date is %d %B %Y') # finds date and time 
         print(dt)
         talk(dt)
     elif 'time' in cmd:
         tm = datetime.datetime.now().strftime('%I %M %p')
         print(tm)
         talk(tm)
+        
+    #plays music
     elif 'play' in cmd:
-        song = cmd.replace('play', '')
+        song = cmd.replace('play', '') # 
         print("Playing ", song)
         pywhatkit.playonyt(song)
+        
+    #random joke
     elif 'joke' in cmd:
         jokes = pyjokes.get_joke()
         talk(jokes)
+   
     elif 'who are you' in cmd:
         engn.say('I am Alexa, your personal assistant.')
         engn.say('What can I do for you?')
         engn.runAndWait()
+        
+    # search wikipedia
     elif 'who is' in cmd:
         prsn = cmd.replace('who is', '')
         info = wikipedia.summary(prsn, 2)
